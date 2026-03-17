7501ae5da5a7a7948651f7b610e4ae3d150b8164
brandonfarley85 <brandonfarley85@gmail.com>
2026-03-17 13:17:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -307,8 +307,10 @@ def home():
 # ===================================================
 
 if __name__ == "__main__":
+    import os
     log.info("BOT STARTED")
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 
 
 
